726116ecdbc5e69202713768c0898df54b9b5f68
Malintha1996 <malintha1996@gmail.com>
2020-07-20 18:40:54 +0530

    Add ui depl

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,7 @@
 set -e
 
 bash -c  "echo \"${DOCKER_PASSWORD}\" | docker login --username malintha1996 --password-stdin"
+
 docker tag zutherb/monolithic-shop:latest malintha1996/monolithic-shop:latest
+
 docker push malintha1996/monolithic-shop:latest
